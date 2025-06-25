3b747e435bd40d1d9238cafadc710fbbf9b25098
shyusu4 <shyusupova4@gmail.com>
2025-06-25 21:55:21 +0300

    Install prettier with jinja template plugin (supports nunjucks too!)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,9 @@
-import globals from "globals";
-import { defineConfig } from "eslint/config";
-
+import globals from 'globals';
+import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    languageOptions: { globals: globals.browser },
+  },
 ]);
